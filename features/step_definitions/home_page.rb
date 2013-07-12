60bedbfc739412f0255c17196a7426f9c768545c
Nicholas Land <nland22@gmail.com>
2013-07-12 10:23:35 -0400

    Added new pictures for the smilies

--- features/step_definitions/home_page.rb
+++ features/step_definitions/home_page.rb
@@ -3,12 +3,12 @@ Given /^I am on the home page$/ do
 end
 
 Then /^I should see three pictures representing my mood$/ do
-  page.should have_xpath("//img[@src=\"/assets/happy.jpeg\"]")
-  page.should have_xpath("//img[@src=\"/assets/undecided.jpeg\"]")
-  page.should have_xpath("//img[@src=\"/assets/sad.jpeg\"]")
+  page.should have_xpath("//img[@src=\"/assets/smiley.jpg\"]")
+  page.should have_xpath("//img[@src=\"/assets/undecided.jpg\"]")
+  page.should have_xpath("//img[@src=\"/assets/sad.jpg\"]")
 end
 
-When /^I select one of the faces$/ do
-  find('#happy')
-  click('#happy')
-end
+# When /^I select one of the faces$/ do
+#   find('#happy')
+#   click('#happy')
+# end
