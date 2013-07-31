86d66089777ce4e30562edf4cb7c812150e37a75
Nicholas Land <nland22@gmail.com>
2013-07-31 12:24:25 -0400

    Fixed JSON repsonses from the server.

--- app/controllers/api/emotions_controller.rb
+++ app/controllers/api/emotions_controller.rb
@@ -1,9 +1,14 @@
 class Api::EmotionsController < ApplicationController
 
   def create
-    emotion = HappinessKpiData.new(emotion: params[:emotion])
-    emotion.save
+    @emotion = HappinessKpiData.new(emotion: params[:emotion])
+    @emotion.save
 
-    render :json => {}
+    render :json => @emotion.emotion
+  end
+
+  def index
+    all_emotions = HappinessKpiData.all
+    render :json => all_emotions.to_json(:only => "emotion")
   end
 end
--- spec/controllers/emotions_controller_spec.rb
+++ spec/controllers/emotions_controller_spec.rb
@@ -2,21 +2,50 @@ require 'spec_helper'
 
 describe Api::EmotionsController do
 
-  it "has a status code of 200 for given emotion" do
-    get :create, emotion: 3
+  describe "#index" do
 
-    response.status.should equal(200)
+    it "fetches all of the emotions" do
+      10.times do
+        Fabricate(:happiness_kpi_data)
+      end
+
+      get :index
+
+      HappinessKpiData.count.should eq(10)
+    end
+
+    it "only renders the emotion field" do
+      Fabricate(:happiness_kpi_data)
+
+      get :index
+
+      theJson = JSON.parse(response.body)
+
+      theJson[0].should have_key("emotion")
+
+      theJson[0].should_not have_key("created_at")
+      theJson[0].should_not have_key("updated_at")
+    end
   end
 
-  it "creates a new record in HappinessKpiData model" do
-    get :create, emotion: 3
+  describe "#create" do
 
-    HappinessKpiData.count.should eq(1)
-  end
+    it "has a status code of 200 for given emotion" do
+      get :create, emotion: 3
 
-  it "stores the correct value for the emotion" do
-    get :create, emotion: 2
+      response.status.should equal(200)
+    end
 
-    HappinessKpiData.first.emotion.should eq(2)
+    it "creates a new record in HappinessKpiData model" do
+      get :create, emotion: 3
+
+      HappinessKpiData.count.should eq(1)
+    end
+
+    it "stores the correct value for the emotion" do
+      get :create, emotion: 2
+
+      HappinessKpiData.first.emotion.should eq(2)
+    end
   end
 end
