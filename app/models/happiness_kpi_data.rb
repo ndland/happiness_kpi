9d5f8bb098569627070fc68d7297940daa177a5c
Nicholas Land <nland22@gmail.com>
2013-08-13 14:30:55 -0400

    Rails HappinessKpi model being validated for a value for the emotion

--- app/models/happiness_kpi_data.rb
+++ app/models/happiness_kpi_data.rb
@@ -1,3 +1,5 @@
 class HappinessKpiData < ActiveRecord::Base
   attr_accessible :emotion, :location
+
+  validates :emotion, :presence => true
 end
--- spec/models/happiness_kpi_data_model_spec.rb
+++ spec/models/happiness_kpi_data_model_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe HappinessKpiData do
 
-  it "exists" do
-    subject.should be
+  it "Validates that there is a value for emotion" do
+    HappinessKpiData.create(:emotion => 2).should be_valid
+  end
+
+  it "Doesn't create the model when there is no emotion specified" do
+    HappinessKpiData.create(:emotion => nil).should_not be_valid
   end
 end
